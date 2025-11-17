989e2bf655417d12864dee6ba53e01dc3f5f3c1d
Maciel-Leonardo <leonardo.g.maciel@outlook.com.br>
2025-11-17 18:50:49 -0300

    Questões 1 a 5

--- Cap4.py
+++ Cap4.py
@@ -1,4 +1,6 @@
 import turtle
+import math
+
 # 1
 def square(t: turtle.Turtle, length: float):
     t.fd(length)
@@ -19,8 +21,34 @@ def polygon(t: turtle.Turtle, length: float, n: int):
         t.lt(Ae)
     turtle.mainloop()
 
-def circle():
+def circle(t, r):
+    circumference = 2 * math.pi * r
+    n = 50
+    length = int(circumference / n)
+    polygon(t, n, length)
     
 
+def arc(t, r, angle):
+    Si = angle * 2 * math.pi * r/360
+    n = 20
+    length = int(Si/n) 
+    Ae=angle/n
+    for i in range(n):
+            t.fd(length)
+            t.lt(Ae)
+    turtle.mainloop()
+
+def flower(t, r, angle):
+    Si = 2 * math.pi * r
+    n = 20
+    length = int(Si/n) 
+    Ae=angle/n
+    for i in range(n):
+            t.fd(length)
+            t.lt(190)
+            t.fd(length)
+    turtle.mainloop()
+   
+
 bob = turtle.Turtle()
-polygon(bob,100,7)
+flower(bob,100,360)
